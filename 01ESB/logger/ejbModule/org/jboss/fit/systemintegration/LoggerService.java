02503a9823fb83d6fbc9dcefc9e406d0049af49b
jsedlace <jsedlace@redhat.com>
2011-10-18 17:40:54 +0200

    Logger webservice

---
+++ 01ESB/logger/ejbModule/org/jboss/fit/systemintegration/LoggerService.java
@@ -0,0 +1,19 @@
+package org.jboss.fit.systemintegration;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(serviceName="Logger")
+@Stateless
+public class LoggerService {
+	
+	@WebMethod(action="name")
+	public void log(@WebParam(name="message") String message) {
+		System.out.println("#########LoggerService log start########");
+		System.out.println(message);		
+		System.out.println("#########LoggerService log end  ########");
+	}
+
+}
