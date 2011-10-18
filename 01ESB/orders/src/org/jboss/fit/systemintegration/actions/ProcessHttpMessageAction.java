53c07b3d900e487e8ad27079bc751b687b8afd2c
jsedlace <jsedlace@redhat.com>
2011-10-18 18:12:13 +0200

    prepared http input and processing

---
+++ 01ESB/orders/src/org/jboss/fit/systemintegration/actions/ProcessHttpMessageAction.java
@@ -0,0 +1,32 @@
+package org.jboss.fit.systemintegration.actions;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.jboss.soa.esb.actions.AbstractActionLifecycle;
+import org.jboss.soa.esb.helpers.ConfigTree;
+import org.jboss.soa.esb.http.HttpRequest;
+import org.jboss.soa.esb.message.Message;
+
+public class ProcessHttpMessageAction extends AbstractActionLifecycle {
+	
+	private ConfigTree config;
+	
+	public ProcessHttpMessageAction(ConfigTree _config) {
+		this.config = _config;
+	}
+
+	public Message processMessage(Message message) {
+		HttpRequest request = HttpRequest.getRequest(message);		
+		Map<String, String[]> params = request.getQueryParams();
+		
+		for (String p : params.keySet()) {
+			String[] values = params.get(p);
+			System.out.println("Param=" + p + ",values=" + Arrays.toString(values));
+		}
+		
+		
+		return message;
+	}
+	
+}
